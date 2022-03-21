3461a69312c55f751704d2376a8ea2e5614c1eaf
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-03-21 18:42:09 +0500

    Added SELECT queries

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES (1,'Agumon', '2020-FEB-03', TRUE, 10.23);
-INSERT INTO animals VALUES (2,'Gabumon', '2018-NOV-15', TRUE, 8);
-INSERT INTO animals VALUES (3,'Pikachu', '2021-JAN-07', FALSE, 15.04);
-INSERT INTO animals VALUES (4,'Devimon', '2017-MAY-12', TRUE, 11);
+INSERT INTO animals VALUES (1,'Agumon', '2020-FEB-03', 0, TRUE, 10.23);
+INSERT INTO animals VALUES (2,'Gabumon', '2018-NOV-15', 2, TRUE, 8);
+INSERT INTO animals VALUES (3,'Pikachu', '2021-JAN-07', 1, FALSE, 15.04);
+INSERT INTO animals VALUES (4,'Devimon', '2017-MAY-12', 5, TRUE, 11);
--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-id serial PRIMARY KEY NOT NULL,              
+id int PRIMARY KEY NOT NULL,              
 name text,
 date_of_birth date,
+escape_attempts int,
 neutered boolean,
 weight_kg decimal
 );
