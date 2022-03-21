ed8dab3eca7b3ccd46c47ab8964daf1f504d72e6
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-03-21 18:10:00 +0500

    Added insertion queries

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (1,'Agumon', '2020-FEB-03', TRUE, 10.23);
+INSERT INTO animals VALUES (2,'Gabumon', '2018-NOV-15', TRUE, 8);
+INSERT INTO animals VALUES (3,'Pikachu', '2021-JAN-07', FALSE, 15.04);
+INSERT INTO animals VALUES (4,'Devimon', '2017-MAY-12', TRUE, 11);
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-id integer,              
+id serial PRIMARY KEY NOT NULL,              
 name text,
 date_of_birth date,
 neutered boolean,
