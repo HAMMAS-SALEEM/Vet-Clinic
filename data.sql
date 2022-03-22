9ef0c2da28fd6c0c7c8a1c50d342aac072da3184
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-03-22 13:38:53 +0500

    Data is inserted

--- data.sql
+++ data.sql
@@ -17,5 +17,75 @@ INSERT INTO animals VALUES (5,'Boarmon', '2005-Jun-07', 7, true, 20.4);
 INSERT INTO animals VALUES (6,'Blossom', '1998-Oct-13', 3, true, 17);
 INSERT INTO animals VALUES (7,'Ditto', '2022-May-14', 4, true, 22);
 
+-- TRANSACTION 01
+
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+-- TRANSACTION 02
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+-- TRANSACTION 03
+
+BEGIN;
+
+DELETE FROM animals;
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+-- TRANSACTION 04
+
+BEGIN;
+
+DELETE FROM animals
+WHERE TO_CHAR(date_of_birth,'YYYY-MON-DD') > '2022-JAN-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg*-1;
+
+ROLLBACK TO SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg*-1
+WHERE weight_kg < 0;
+
+COMMIT;
+
 -- Third Project
 
+-- Owners Information
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+-- Species Information
+
+INSERT INTO species(name) VALUES ('Digimon');
+INSERT INTO species(name) VALUES ('Pokemon');
